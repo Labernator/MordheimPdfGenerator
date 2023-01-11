ac9fb1857a7b90a5ed399fb546b3ea2308ea9214
Marius Laber <marius.laber@sap.com>
2023-01-11 13:28:23 +0100

    Update GithubFilePicker.tsx

--- src/components/GithubFilePicker.tsx
+++ src/components/GithubFilePicker.tsx
@@ -13,19 +13,22 @@ export const GithubFilePicker = () => {
     const [currentContentUrl, setCurrentContentUrl] = useState("");
     const [currentContent, setCurrentContent] = useState<GithubContent[]>([]);
     const [selectedFile, setSelectedFile] = useState<string>();
+    const [navigationMap, setNavigationMap] = useState<GithubContent[]>([]);
     const history = useHistory();
     useEffect(() => {
-        const getUsers = async () => fetchData();
+        const fetching = async () => fetchData();
         // tslint:disable-next-line: no-floating-promises
-        getUsers();
+        fetching();
         return () => {
             // this now gets called when the component unmounts
         };
     }, [currentContentUrl]);
     const fetchData = async () => {
-        // tslint:disable-next-line: strict-string-expressions
         const response = await fetch(currentContentUrl || `https://api.github.com/repos/${githubUser}/${githubRepo}/contents`);
         const jsonResponse = await response.json();
+        if (!currentContentUrl) {
+            setNavigationMap([{ ...jsonResponse, name: "root" }]);
+        }
         setCurrentContent(jsonResponse);
     };
     const getYamlDataAndJump = async () => {
@@ -34,9 +37,23 @@ export const GithubFilePicker = () => {
         }
         const response = await fetch(selectedFile);
         const txtResponse = await response.text();
-        // const jsonResponse = yaml.load(response as unknown as string);
         history.push("/PdfExport", yaml.load(txtResponse));
     };
+    const getNavigationRoute = () =>
+        navigationMap.length > 0 ?
+            <div style={{ display: "flex" }}>
+                {navigationMap.map((nav, idx) =>
+                    <div
+                        style={{ color: "blue", padding: "1rem 0.1rem" }}
+                        onClick={() => {
+                            setCurrentContentUrl(nav.url);
+                            setNavigationMap(navigationMap.slice(0, idx + 1));
+                        }}>
+                        {`${nav.name} /`}
+                    </div>)}
+            </div> :
+            <div>No Path yet</div>;
+
     const getFoldersAndYmls = () => {
         const filteredContent = currentContent.length && currentContent.length > 0 ? currentContent.filter((content) => content.type === "dir" || (content.type === "file" && content.name.indexOf(".yml") !== -1)) : undefined;
         return <table className="storage-table" style={{ width: "calc(100% - 1.5rem)", textAlign: "left" }}>
@@ -45,7 +62,14 @@ export const GithubFilePicker = () => {
             </thead>
             <tbody>
                 {filteredContent ? filteredContent.map((content) => content.type === "dir" ?
-                    <tr onClick={() => setCurrentContentUrl(content.url)}><td>{content.name}</td><td>Dir</td></tr> :
+                    <tr onClick={() => {
+                        setCurrentContentUrl(content.url);
+                        setNavigationMap([...navigationMap, content]);
+                    }
+                    }>
+                        <td>{content.name}</td>
+                        <td>Dir</td>
+                    </tr> :
                     <tr onClick={() => setSelectedFile(content.download_url)} className={content.download_url === selectedFile ? "selected" : ""}><td>{content.name}</td><td>File</td></tr>
                 ) : undefined}
             </tbody>
@@ -57,7 +81,8 @@ export const GithubFilePicker = () => {
         <div className="text-with-margins" style={{ float: "left" }}>{githubUser}/{githubRepo}</div>
     </div>
         <div>
-            <div className="bold-text-with-margins" style={{ marginTop: "1rem" }}>Repository content (tap to navigate)</div>
+            <div className="bold-text-with-margins" style={{ marginTop: "1rem" }}>Tap on an entry in the table to navigate or on the path below to go back</div>
+            {getNavigationRoute()}
             {getFoldersAndYmls()}
             <div className={selectedFile ? "enabled" : "disabled"} onClick={getYamlDataAndJump}>Generate Pdf from Selection</div>
 
